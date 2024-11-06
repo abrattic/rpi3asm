da2897d3ef15c21967ef1d49afad6a7d74eb4a1b
Shreshth <69515858+abrattic@users.noreply.github.com>
2024-11-06 12:38:27 +0530

    Blinking an LED

---
+++ main.s
@@ -0,0 +1,39 @@
+.global _start
+
+.equ GPIO_BASE, 0x3f200000
+.equ GPIO_FSEL2, 0x08
+.equ FSEL_21_OUTPUT, 0b001000
+.equ GPIO_SET0, 0x1c
+.equ GPIO_CLR0, 0x28
+
+# 1 << 21
+.equ GPIO_21, 0x200000
+
+_start:
+	ldr r0, =GPIO_BASE
+
+	ldr r1, =FSEL_21_OUTPUT	
+	str r1, [r0, #GPIO_FSEL2]
+
+	ldr r1, =GPIO_21
+
+	mov r2, #0x800000
+loop:
+	str r1, [r0, #GPIO_SET0]
+
+	mov r10, #0
+	delay1:
+		add r10, r10, #1
+		cmp r10, r2
+		bne delay1
+
+	str r1, [r0, #GPIO_CLR0]
+
+	mov r10, #0
+	delay2:
+		add r10, r10, #1
+		cmp r10, r2
+		bne delay2
+
+	b loop
+
